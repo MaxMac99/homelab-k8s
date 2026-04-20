9eb6dfcb8bd5c397bbd94e20a92fad919d7ceb55
Max Vissing <max_vissing@yahoo.de>
2026-04-20 16:16:52 +0200

    Add mosquitto

--- apps/index.ts
+++ apps/index.ts
@@ -6,6 +6,7 @@ import "./homepage";
 import "./unifi";
 import "./adguard";
 import "./homeassistant";
+import "./mosquitto";
 import "./timemachine";
 
 export * from "./paperless";
@@ -13,4 +14,5 @@ export * from "./homepage";
 export * from "./unifi";
 export * from "./adguard";
 export * from "./homeassistant";
+export * from "./mosquitto";
 export * from "./timemachine";
